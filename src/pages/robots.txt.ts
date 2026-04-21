ea527a0374142a2c8dc56c6d1efa33aed175bc64
Mansukh Patidar <mansukh.patidar@gmail.com>
2026-04-21 22:21:49 +0530

    trigger deployment

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -1,5 +1,6 @@
 import type { APIRoute } from "astro";
 
+
 export const GET: APIRoute = ({ site }) => {
   const sitemap = new URL("sitemap-index.xml", site).href;
   const body = `User-agent: *\nAllow: /\n\nSitemap: ${sitemap}\n`;
